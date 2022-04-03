3c5d6e6dc07131dc2836d19ef6b2e94dcb918635
Niken-Amalia <nknaml05@gmail.com>
2022-04-03 15:35:15 +0700

    menambahkan file koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "" ;
+$dbname = "niken";
+
+$koneksi = mysqli_connect($host,$user,$password,$dbname);
+//if(!$koneksi){
+//   echo "koneksi gagal";
+//}else{
+   //echo "koneksi berhasil";
+//}
+?>
